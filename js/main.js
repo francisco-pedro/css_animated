8e7333304c8b80939250c86053021936305cb882
Francisco Pedro <69425464+francisco-pedro@users.noreply.github.com>
2021-05-29 23:03:02 +0100

    Refatorando o código

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
-var sun = document.querySelector('.sun');
-var body = document.querySelector('body');
-
-sun.onclick = function(){
-    body.classList.toggle('dark')
+function changeBackground(sunClass, bodyTag){
+const sun = document.querySelector('.sun'),
+    body = document.querySelector('body')
+if(sun){
+    sun.addEventListener('click', () =>{
+        body.classList.toggle('dark')
+    })
 }
+
+}
+
+changeBackground('.sund', 'body')
